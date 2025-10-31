08ae64c01abb26fee20e4c02dd7e4ea041a567b6
Marat Rubin <maratrubin@MacBook-Pro.local>
2025-10-31 17:00:41 -0400

    Исправлена зависимость TelegramModule от StorageModule для поддержки отправки медиа

--- src/messengers/telegram/telegram.module.ts
+++ src/messengers/telegram/telegram.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TelegramService } from './telegram.service';
+import { StorageModule } from '../../storage/storage.module';
 
 @Module({
+  imports: [StorageModule],
   providers: [TelegramService],
   exports: [TelegramService],
 })
